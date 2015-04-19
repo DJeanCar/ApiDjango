66fdf86f97b3dead8e3b79332316da6bbfea0ea9
jean <jean_carlos104@hotmail.com>
2015-04-19 15:29:13 -0500

    a

--- apps/main/serializers.py
+++ apps/main/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Notice, Comment
+from .models import Notice, Comment, Category, Author
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -8,7 +8,28 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 		model = User
 		fields = ('username',)
 
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
+	class Meta:
+		model = Category
+		fields = ('name',)
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+
+	class Meta:
+		model = Author
+		fields = ('name',)
+
 class NoticeSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Notice
+		fields = ('id', 'title', 'resumen', 'main', 'image')
+
+class NoticeDetailSerializer(serializers.ModelSerializer):
+
+	category = CategorySerializer()
+	author = AuthorSerializer()
 
 	class Meta:
 		model = Notice
